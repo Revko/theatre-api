39df7c37ee6ccc4a724798d0e174f7221574ed81
Ivan <revko7692@gmail.com>
2025-07-03 21:56:15 +0300

    add custom user to models

--- theatre/models.py
+++ theatre/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=64)
@@ -36,3 +37,14 @@ class Ticket(models.Model):
 
     class Meta:
         unique_together = ("performance", "row", "seat")
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
