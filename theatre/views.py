19217747bfd681ed66a94bc19e9d9a5e1421b268
Ivan <revko7692@gmail.com>
2025-07-03 21:49:47 +0300

    add views

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from .serializers import *
 
-# Create your views here.
+class PlayViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+
+class PerformanceViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+
+class TheatreHallViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+
+class ReservationViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
