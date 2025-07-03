4a18d489fb2153e13b7ea858e07918784d2dc77a
Ivan <revko7692@gmail.com>
2025-07-03 21:47:12 +0300

    add class Meta to models

--- theatre/models.py
+++ theatre/models.py
@@ -1,15 +1,12 @@
-from django.contrib.auth import get_user_model
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
 
-
 class Genre(models.Model):
     name = models.CharField(max_length=64)
-
 
 class Play(models.Model):
     title = models.CharField(max_length=128)
@@ -17,26 +14,25 @@ class Play(models.Model):
     actors = models.ManyToManyField(Actor)
     genres = models.ManyToManyField(Genre)
 
-
 class TheatreHall(models.Model):
     name = models.CharField(max_length=64)
-    rows = models.IntegerField()
-    seats_in_row = models.IntegerField()
-
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
 
 class Performance(models.Model):
     play = models.ForeignKey(Play, on_delete=models.CASCADE)
     theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
     show_time = models.DateTimeField()
 
-
 class Reservation(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
-
 class Ticket(models.Model):
     performance = models.ForeignKey(Performance, on_delete=models.CASCADE)
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
-    row = models.IntegerField()
-    seat = models.IntegerField()
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ("performance", "row", "seat")
