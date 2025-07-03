118ce77fa1163e4851fba30198ffe5aa7387c0c1
Ivan <revko7692@gmail.com>
2025-07-03 21:45:19 +0300

    add DEFAULT_SCHEMA_CLASS  settings

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,16 @@ INSTALLED_APPS = [
     "drf_spectacular"
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Theatre API",
+    "DESCRIPTION": "Booking API for Theatre",
+    "VERSION": "1.0.0",
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
