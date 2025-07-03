aab7d2ac285bbb600f878d57c888f156b03fe52d
Ivan <revko7692@gmail.com>
2025-07-03 22:28:46 +0300

    regiser models in admin.py

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import (
+    User,
+    Actor,
+    Genre,
+    Play,
+    TheatreHall,
+    Performance,
+    Reservation,
+    Ticket,
+)
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "is_staff"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
+        }),
+    )
+    search_fields = ("email",)
+    model = User
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    filter_horizontal = ("actors", "genres")
+
+@admin.register(TheatreHall)
+class TheatreHallAdmin(admin.ModelAdmin):
+    list_display = ("name", "rows", "seats_in_row")
+
+@admin.register(Performance)
+class PerformanceAdmin(admin.ModelAdmin):
+    list_display = ("play", "theatre_hall", "show_time")
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at")
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("performance", "reservation", "row", "seat")
